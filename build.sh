70f4d9822bc6760844cc8a62d8ecc5a0eb37b0e2
Sean McKenna <mckennapsean@gmail.com>
2016-05-10 15:14:21 -0600

    add daf paper to the dissertation, need to break apart though

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 # build tex files
 pandoc -F pandoc-crossref -M "crossrefYaml=styles/pandoc-crossref.yaml" --natbib --chapters chapters/bubble-net.md -o chapters/bubble-net.tex
+pandoc -F pandoc-crossref -M "crossrefYaml=styles/pandoc-crossref.yaml" --natbib --chapters chapters/daf.md -o chapters/daf.tex
 pandoc front/abstract.md -o front/abstract.tex
 pandoc front/acknowledge.md -o front/acknowledge.tex
 
