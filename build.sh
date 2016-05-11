0a2d84ff6ac9cb0ef85a4e4f00299cf9a0688001
Sean McKenna <mckennapsean@gmail.com>
2016-05-11 12:32:25 -0600

    update dissertation to include and build scorrplot paper

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 # build tex files
 pandoc -F pandoc-crossref -M "crossrefYaml=styles/pandoc-crossref.yaml" --natbib --chapters chapters/bubble-net.md -o chapters/bubble-net.tex
 pandoc -F pandoc-crossref -M "crossrefYaml=styles/pandoc-crossref.yaml" --natbib --chapters chapters/daf.md -o chapters/daf.tex
+pandoc -F pandoc-crossref -M "crossrefYaml=styles/pandoc-crossref.yaml" --natbib --chapters chapters/scorrplot.md -o chapters/scorrplot.tex
 pandoc front/abstract.md -o front/abstract.tex
 pandoc front/acknowledge.md -o front/acknowledge.tex
 
