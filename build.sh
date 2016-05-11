ec29a92f1811b241f2c85b94ee204ae35f491d89
Sean McKenna <mckennapsean@gmail.com>
2016-05-11 13:44:13 -0600

    add dissertation to include design methods for cyber security paper too now

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 # build tex files
 pandoc -F pandoc-crossref -M "crossrefYaml=styles/pandoc-crossref.yaml" --natbib --chapters chapters/bubble-net.md -o chapters/bubble-net.tex
+pandoc -F pandoc-crossref -M "crossrefYaml=styles/pandoc-crossref.yaml" --natbib --chapters chapters/methods.md -o chapters/methods.tex
 pandoc -F pandoc-crossref -M "crossrefYaml=styles/pandoc-crossref.yaml" --natbib --chapters chapters/daf.md -o chapters/daf.tex
 pandoc -F pandoc-crossref -M "crossrefYaml=styles/pandoc-crossref.yaml" --natbib --chapters chapters/scorrplot.md -o chapters/scorrplot.tex
 pandoc front/abstract.md -o front/abstract.tex
