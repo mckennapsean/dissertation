7c22b494efdafde55aff20a011b1a63c7974d5fe
Sean McKenna <mckennapsean@gmail.com>
2016-05-11 14:37:53 -0600

    clean up proposal output, just get the main document and content for now, plus include intro

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #/bin/sh
 
 # build tex files
+pandoc -F pandoc-crossref -M "crossrefYaml=styles/pandoc-crossref.yaml" --natbib --chapters chapters/intro.md -o chapters/intro.tex
 pandoc -F pandoc-crossref -M "crossrefYaml=styles/pandoc-crossref.yaml" --natbib --chapters chapters/bubble-net.md -o chapters/bubble-net.tex
 pandoc -F pandoc-crossref -M "crossrefYaml=styles/pandoc-crossref.yaml" --natbib --chapters chapters/methods.md -o chapters/methods.tex
 pandoc -F pandoc-crossref -M "crossrefYaml=styles/pandoc-crossref.yaml" --natbib --chapters chapters/daf.md -o chapters/daf.tex
