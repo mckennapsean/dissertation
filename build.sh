4f3fd4c33f30d5eb90315e5036730d14392f7ded
Sean McKenna <mckennapsean@gmail.com>
2017-01-16 22:21:45 -0700

    fix task and shell script on windows

--- build.sh
+++ build.sh
@@ -10,14 +10,12 @@ if [ "$1" == "-c" ]; then
 else
 
 # build tex files from markdown
-chapters=($(find chapters -type f -name "*.md"))
-for chapter in ${chapters[*]}
+for chapter in chapters/*.md
 do
   pandoc -F pandoc-fignos --natbib --top-level-division=chapter $chapter -o ${chapter%.md}.tex
 done
 
-# front=($(find front -type f -name "*.md"))
-# for item in ${front[*]}
+# for item in front/*.md
 # do
 #   pandoc -F pandoc-fignos --natbib --top-level-division=chapter $item -o ${item%.md}.tex
 # done
