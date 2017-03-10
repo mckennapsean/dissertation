53999d5057b23dcfdc9e48cb873cbfd76fbb7904
Sean McKenna <mckennapsean@gmail.com>
2017-03-10 14:49:07 -0700

    merge diss content into one folder for simplicity

--- build.sh
+++ build.sh
@@ -3,18 +3,11 @@
 # clean all files when given "-c" flag, otherwise build pdf
 if [ "$1" == "-c" ]; then
   latexmk -C
-  rm chapters/*.tex
-  rm chapters/*.aux
   rm content/*.tex
   rm content/*.aux
 else
 
 # build tex files from markdown
-for chapter in chapters/*.md
-do
-  pandoc -F pandoc-fignos --natbib --top-level-division=chapter $chapter -o ${chapter%.md}.tex
-done
-
 for item in content/*.md
 do
   pandoc -F pandoc-fignos --natbib --top-level-division=chapter $item -o ${item%.md}.tex
