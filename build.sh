4729211fd18e5abbae312c1a5ab8b6da62f3d25d
Sean McKenna <mckennapsean@gmail.com>
2017-03-10 15:26:17 -0700

    update build folders to ease editing

--- build.sh
+++ build.sh
@@ -1,18 +1,21 @@
 #/bin/sh
 
 # variables for building and cleaning
-TEMP="tex"
+TEMP="tmp"
+TEX="tex"
 FILE="thesis"
 
 # clean latexmk files when given "-c" flag, otherwise build pdf
 if [ "$1" == "-c" ]; then
   rm -rf $TEMP
+  rm -rf $TEX
 else
 
 # build tex files from markdown (overwrites each time)
+mkdir $TEX
 for ITEM in content/*.md
 do
-  pandoc -F pandoc-fignos --natbib --top-level-division=chapter $ITEM -o ${ITEM%.md}.tex
+  pandoc -F pandoc-fignos --natbib --top-level-division=chapter $ITEM -o $TEX/$(basename ${ITEM%.md}).tex
 done
 
 # generate pdf using latexmk
